64622a419e756027a49640cf8117e0099191d508
choiinyoung <choi121100@naver.com>
2023-04-09 17:04:48 +0900

    feat:테이블 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Headstyle = styled.div`
+  height: 12vh;
+  background-color: #446a72;
+`;
+
+export default function Header() {
+  return <Headstyle>헤더</Headstyle>;
+}
