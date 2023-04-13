1dad9d4e55eb0fa71ae6a20b1dcf76fc36a1587d
김정혁 <jack1150@naver.com>
2023-04-13 14:32:32 +0900

    loging token 진행중

--- src/App.js
+++ src/App.js
@@ -8,27 +8,28 @@ import Register from './pages/Register';
 import KakaoRedirectHandler from './components/KakaoRedirectHandler';
 import UserMain from './pages/UserMain';
 import UserRent from './pages/UserRent';
+import FindRobot from './components/FindRobot';
 
 function App() {
   const isLogin = useSelector((state) => state.user.isLogin);
   return (
     <>
       <GlobalStyle />
+      {/* <Header />
+      <UserMainMenu />
+      <UserRent /> */}
       <Routes>
-        <Route path="/" element={isLogin ? <UserMain /> : <Login />} />
+        {/* <Route path="/myPage" element={<UserMyPage />} /> */}
+        <Route path="/" element={<FindRobot />} />
+        {/* <Route path="/" element={isLogin ? <UserMain /> : <Login />} />
         <Route path="/register" element={isLogin ? <Login /> : <Register />} />
         <Route
           path="/oauth/callback/kakao"
           element={<KakaoRedirectHandler />}
-        />
+        /> */}
 
-        {/* <Route path="/userrent" element={<UserRent />} /> */}
-
-        <Route path="/usermain" element={<UserMain />} />
-
-        {/* <Route path="/" element={<UserMain />} /> */}
-        {/* <Route path="/main" element={<UserMain />} /> */}
-        <Route path="/subMain/:id" element={<UserRent />} />
+        {/* <Route path="/usermain" element={<UserMain />} />
+        <Route path="/subMain/:id" element={<UserRent />} /> */}
       </Routes>
     </>
   );
