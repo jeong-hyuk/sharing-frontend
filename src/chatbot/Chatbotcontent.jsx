c2c00a47eff66deff0110383af103aa0c5b099ec
choiinyoung <choi121100@naver.com>
2023-04-14 11:22:59 +0900

    챗봇 완료

--- src/chatbot/Chatbotcontent.jsx
+++ src/chatbot/Chatbotcontent.jsx
@@ -1,28 +1,90 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faComments,
+  faCircleQuestion,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import styled from 'styled-components';
+import config from '../chatbot/config';
+import Chatbot from 'react-chatbot-kit';
+import MessageParser from '../chatbot/MessageParser';
+import ActionProvider from '../chatbot/ActionProvider';
+import 'react-chatbot-kit/build/main.css';
 
 const Mainstyle = styled.div`
-  position: relative;
+  //챗봇 전체 스타일 관리
   .chatbotallcontroller {
-    position: absolute;
-    transform: translate(75vw, 35vh);
-    width: 23vw;
-    height: 60vh;
-    background-color: #ffa6004a;
-    border: 1px solid black;
-    border-radius: 10%;
-  }
-  .footer {
-    position: fixed;
-    width: 23vw;
-    height: 12vh;
-    background-color: #c4040449;
-    ol {
-      li {
-        p.icon {
-          font-size: 3rem;
+    overflow: hidden;
+    transform: translate(50vw, 22vh);
+    width: 20vw;
+    height: 70vh;
+    color: #fff;
+    box-shadow: 0 0 0.5px 0.5px gray;
+    border-radius: 15px;
+    .on {
+      display: block;
+    }
+    // 첫 화면에 나오는 페이지
+    .main {
+      width: 100%;
+      height: 85%;
+      background: linear-gradient(black, #d4d2d2, #fff);
+      .header {
+        display: flex;
+        height: 15%;
+        justify-content: space-around;
+        p {
+          font-size: 5rem;
+          transform: translateX(2vw);
+        }
+      }
+      .content {
+        height: 30%;
+        p {
+          font-size: 2.5rem;
+          font-weight: 700;
+          transform: translate(2.5vw, 5vh);
+        }
+      }
+    }
+    // 두번쨰 챗봇 부분
+    .msg {
+      height: 85%;
+      .msgcontroller {
+        height: 50%;
+        .react-chatbot-kit-chat-container {
+          width: 20vw;
+          height: 60vh;
+        }
+        .react-chatbot-kit-chat-message-container {
+          padding: 0 17.5px 10px 17.5px;
+          overflow: scroll;
+          height: 50vh;
+        }
+      }
+    }
+    // 밑에 있는 3개의 버튼
+    .footer {
+      position: fixed;
+      width: 100%;
+      height: 15%;
+      color: black;
+      border-top: gray;
+      border-radius: 0 0 35px 35px;
+      z-index: 10;
+      ol {
+        display: flex;
+        justify-content: space-around;
+        font-size: 14px;
+        border-top: 1px solid rgba(0, 0, 0, 0.05);
+        box-shadow: rgba(237, 237, 237, 0.05) 0px 0px 25px;
+        text-align: center;
+        li {
+          p.icon {
+            font-size: 3rem;
+          }
         }
       }
     }
@@ -37,17 +99,29 @@ export default function Chatbotcontent() {
           <div className="main on">
             <div className="header">
               <img src="-" alt="로고" />
-              <img src="-" alt="사용자" />
+              <p className="icon">
+                <FontAwesomeIcon icon={faUser} />
+              </p>
             </div>
-            <h1>
-              Hello 인영,
-              <br />
-              How can we help?
-            </h1>
+            <div className="content">
+              <p>
+                Hello 인영,
+                <br />
+                How can we help?
+              </p>
+            </div>
             <div>달력</div>
           </div>
-          {/* <div className="msg"></div>
-        <div className="help"></div> */}
+          {/* <div className="msg">
+            <div className="msgcontroller">
+              <Chatbot
+                config={config}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+              />
+            </div>
+          </div> */}
+          <div className="help"></div>
           <div className="footer">
             <ol>
               <li>
@@ -57,12 +131,16 @@ export default function Chatbotcontent() {
                 <p>Home</p>
               </li>
               <li>
-                <span className="material-symbols-outlined">forum</span>
-                <span>Messages</span>
+                <p className="icon">
+                  <FontAwesomeIcon icon={faComments} />
+                </p>
+                <p>Messages</p>
               </li>
               <li>
-                <span className="material-symbols-outlined">help</span>
-                <span>Help</span>
+                <p className="icon">
+                  <FontAwesomeIcon icon={faCircleQuestion} />
+                </p>
+                <p>Help</p>
               </li>
             </ol>
           </div>
